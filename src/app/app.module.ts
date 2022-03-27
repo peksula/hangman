f5e600627ae1cecda437d2eb6da95c522e7431ee
Pekka Tukiainen <pekka.t.tukiainen@gmail.com>
2022-03-27 19:05:44 +0300

    add container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HangmanComponent } from './hangman/hangman.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HangmanComponent
   ],
   imports: [
     BrowserModule,
